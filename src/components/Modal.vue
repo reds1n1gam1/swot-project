995a8417b1ffc8e1d8632cf3a1123ebf32578ad8
Madi <reds1n1gam1@gmail.com>
2026-02-18 01:56:58 +0200

    Mobile iOSA bug fix test 1

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -29,15 +29,13 @@ const props = defineProps({
 <style scoped>
 .modal-mask {
     position: fixed;
-    z-index: 9998;
-    top: 0;
-    left: 0;
+    z-index: 9999;
+    inset: 0;
     width: 100%;
     height: 100%;
     background-color: rgba(0, 0, 0, 0.5);
     display: flex;
-    transform: translateZ(0);
-    /* transition: opacity 0.3s ease; */
+    transition: opacity 0.3s ease;
 }
 
 .modal-container {
@@ -47,7 +45,7 @@ const props = defineProps({
     background-color: var(--primary-bg-color);
     border-radius: 2px;
     box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
-    /* transition: all 0.3s ease; */
+    transition: all 0.3s ease;
 }
 
 .modal-header h3 {
