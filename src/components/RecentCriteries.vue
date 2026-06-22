5639d7688c10efecf9aabc6da23c0fea5d864212
reds1n1gam1 <reds1n1gam1@gmail.com>
2026-06-22 15:47:48 +0500

    feat: updated recent criterias block

--- src/components/RecentCriteries.vue
+++ src/components/RecentCriteries.vue
@@ -6,22 +6,25 @@
         </div>
 
         <div class="card">
-            <DataTable :value="listItem" tableStyle="">
+            <DataTable :value="factorsList" tableStyle="">
                 <Column field="title" header="Title"></Column>
-                <Column field="category" header="Category"></Column>
+                <Column field="type" header="Type"></Column>
                 <Column field="priority" header="Quantity"></Column>
                 <Column>
                     <template #body="{ data }">
-                        <Button icon="pi pi-edit" @click="editRow(data)" severity="secondary" rounded></Button>
+                        <Button icon="pi pi-pen-to-square" @click="editRow(data)" severity="secondary" rounded></Button>
                     </template>
                 </Column>
 
                 <Column>
                     <template #body="{ data }">
-                        <Button icon="pi pi-delete" @click="removeRow(data)" severity="secondary" rounded></Button>
+                        <Button icon="pi pi-trash" @click="removeRow(data)" severity="secondary" rounded></Button>
                     </template>
                 </Column>
             </DataTable>
+
+            <Button type="button" :label="'View all criteria(' + store.getFactorsLength + ')'" icon="pi pi-arrow-right" class="w-full" />
+
         </div>
     </div>
 </template>
@@ -29,10 +32,14 @@
 <script setup lang="ts">
 import DataTable from 'primevue/datatable';
 import Column from 'primevue/column';
-import { ref, type Ref } from 'vue';
+import { onMounted, ref, type Ref } from 'vue';
 import type { FactorItem } from '../types/FactorItem';
+import { useFactorsStore } from '../store/factors-store';
+import { Button } from 'primevue';
 
-const listItem: Ref<FactorItem[]>= ref([]);
+const store = useFactorsStore()
+
+const factorsList: Ref<FactorItem[]>= ref([]);
 
 function editRow(data:FactorItem) {
 
@@ -41,6 +48,10 @@ function editRow(data:FactorItem) {
 function removeRow(data:FactorItem) {
 
 }
+
+onMounted(() => {
+    factorsList.value = store.getRecentFactors
+})
 </script>
 
 <style scoped></style>
