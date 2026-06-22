89f0503a3561f4a9c55b0517eca121f6e926aa65
reds1n1gam1 <reds1n1gam1@gmail.com>
2026-06-22 14:53:46 +0500

    feat: recent criteria

--- src/components/RecentCriteries.vue
+++ src/components/RecentCriteries.vue
@@ -4,13 +4,42 @@
             <i class="pi pi-history mr-2"></i>
             <p class="font-semibold text-lg">Recent criteries</p>
         </div>
+
+        <div class="card">
+            <DataTable :value="listItem" tableStyle="">
+                <Column field="title" header="Title"></Column>
+                <Column field="category" header="Category"></Column>
+                <Column field="priority" header="Quantity"></Column>
+                <Column>
+                    <template #body="{ data }">
+                        <Button icon="pi pi-edit" @click="editRow(data)" severity="secondary" rounded></Button>
+                    </template>
+                </Column>
+
+                <Column>
+                    <template #body="{ data }">
+                        <Button icon="pi pi-delete" @click="removeRow(data)" severity="secondary" rounded></Button>
+                    </template>
+                </Column>
+            </DataTable>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+import { ref } from 'vue';
 
+const listItem = ref();
+
+function editRow(data:unknown) {
+
+}
+
+function removeRow(data:unknown) {
+
+}
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
