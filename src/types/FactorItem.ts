41434db342aec22f761fe40d1c8fe2f387cd134c
reds1n1gam1 <reds1n1gam1@gmail.com>
2026-06-22 15:01:56 +0500

    feat: added main types

---
+++ src/types/FactorItem.ts
@@ -0,0 +1,9 @@
+import type { FactorType } from "./FactorType";
+import type { PriorityType } from "./PriorityType";
+
+export interface FactorItem {
+  id: number;
+  title: string;
+  type: FactorType;
+  priority: PriorityType;
+}
